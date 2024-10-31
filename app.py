e187df7bc624674d4bacbcd374b625a6bcbbc439
scarscarin <scarscarin@gmail.com>
2024-10-31 12:16:55 +0100

    updated py and html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
@@ -13,22 +13,43 @@ twilio_phone_number = os.getenv('TWILIO_PHONE_NUMBER')
 
 client = Client(account_sid, auth_token)
 
-phone_number = "+34602210973" 
+people = {
+    "Marta": "+34602210973",
+    "Leo": "+31642153785",
+    "Spirit": "+3197008249167"
+}
 
-audio_file_url = "https://github.com/scarscarin/halloween-audio-files/raw/refs/heads/main/spirit_announcement.wav" 
+audio_files = {
+    "Spirit Announcement": "https://github.com/scarscarin/halloween-audio-files/raw/refs/heads/main/audios/spirit_announcement.wav",
+    "Claiming Murder": "https://github.com/scarscarin/halloween-audio-files/raw/refs/heads/main/audios/spirit_announcement.wav"
+}
 
 # Flask app to handle the trigger
 app = Flask(__name__)
 
-@app.route('/trigger_call', methods=['GET'])
-def trigger_call():
+
+# Route to display the interface
+@app.route('/')
+def index():
+    return render_template('index.html', people=people, audio_files=audio_files)
+
+# Route to handle the call
+@app.route('/make_call', methods=['POST'])
+def make_call():
+    person = request.form['person']
+    audio = request.form['audio']
+
+    # Get the phone number and audio URL
+    phone_number = people[person]
+    audio_file_url = audio_files[audio]
+
     # Make the call
     call = client.calls.create(
         to=phone_number,
         from_=twilio_phone_number,
         url=f"https://handler.twilio.com/twiml/EHbc8d282a68f14d8a4938d4444d9c8ee0?AudioFileUrl={audio_file_url}"
     )
-    return f"Call initiated to {phone_number} with audio file {audio_file_url}"
+    return f"Call initiated to {person} ({phone_number}) with audio '{audio}'"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
